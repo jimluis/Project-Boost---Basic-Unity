5af4004bde4dcf2968f19413eac9b88a3aee68c1
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-04-09 18:32:43 -0400

    Partial game design

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -9,7 +9,12 @@ public class Rocket : MonoBehaviour
     Rigidbody rigidbody;
     AudioSource audioSource;
     bool isAudioPlaying = false;
-
+    // [SerializeField] makes the variable as a member variable, and it acts as public, 
+    // it can be accessed from the inspector, but
+    // the only difference is that this variable can not be
+    // modified from other scripts 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float thrustPower = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,18 +25,41 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    void ProcessInput()
+    void Rotate()
+    {
+        rigidbody.freezeRotation = true; // Take manual control of rotatio
+        float rotationThisFrame = rcsThrust * Time.deltaTime; //Time frame independent
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rcsThrust: "+ rcsThrust + " - Time.deltaTime: " + Time.deltaTime);
+            print("rotationThisFrame: " + rotationThisFrame);
+            print("transform.Rotate: " + Vector3.forward * rotationThisFrame);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false; // resumes physics control of rotation
+    }
+
+    void Thrust()
     {
 
-
+        //float tPower = thrustPower * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space))
         {
             print("Thrusting");
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * thrustPower);
 
 
             if (!audioSource.isPlaying)
@@ -42,19 +70,28 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
 
-        // audioSource.Stop();
 
-        if (Input.GetKey(KeyCode.A))
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Collision");
+
+        switch(collision.gameObject.tag)
         {
-            print("Rotating left");
-            transform.Rotate(Vector3.forward);
+            case "Friendly":
+                print("OK");
+                break;
+            case "Platform":
+                print("OK");
+                break;
+            case "Ground":
+                print("Dead");
+                break;
+            default:
+                print("Dead");
+                break;
         }
 
-        else if(Input.GetKey(KeyCode.D))
-        {
-            print("Rotating Right");
-            transform.Rotate(-Vector3.forward);
-        }
     }
 }
